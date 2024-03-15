7aae0f3da6e936844d454af64bf52cb98784a48a
chi1034 <odiigift12@gmail.com>
2024-03-14 19:11:36 -0700

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/0-import_add.py
@@ -0,0 +1,12 @@
+mport_add.py
+
+import add_0
+
+# Assign the value 1 to a variable called a
+a = 1
+
+# Assign the value 2 to a variable called b
+b = 2
+
+# Call the function add and print the result with string format
+print(f"{a} + {b} = {add_0.add(a, b)}")
